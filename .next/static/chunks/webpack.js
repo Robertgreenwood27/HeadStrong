e0b00cd6783c9ceaa396c6cc8eae1f41ed02ff08
Robert Greenwood <RobertGreenwood27@gmail.com>
2023-03-05 10:01:58 -0700

    mobile improvements

--- .next/static/chunks/webpack.js
+++ .next/static/chunks/webpack.js
@@ -131,7 +131,7 @@
 /******/ 	
 /******/ 	/* webpack/runtime/getFullHash */
 /******/ 	!function() {
-/******/ 		__webpack_require__.h = function() { return "88830201615d33a4"; }
+/******/ 		__webpack_require__.h = function() { return "5f59c368a334731e"; }
 /******/ 	}();
 /******/ 	
 /******/ 	/* webpack/runtime/global */
--- components/Footer.js
+++ components/Footer.js
@@ -5,40 +5,40 @@ import { FaFacebookF, FaInstagram, FaTwitter } from 'react-icons/fa';
 export default function Footer() {
   return (
     <footer className="bg-zinc-900 text-zinc-300">
-      <div className="container mx-auto px-4 py-8">
-        <h2 className="text-lg font-bold mb-4">Connect with us</h2>
-        <div className="flex flex-col md:flex-row md:justify-between md:items-center">
+      <div className="container mx-auto px-4 py-8 flex flex-col items-center justify-center md:text-center">
+        <h2 className="text-lg font-bold mb-4 text-center">Connect with us</h2>
+        <div className="flex flex-col md:flex-row md:items-center justify-between w-full max-w-4xl">
           <div className="flex flex-col md:flex-row md:items-center">
             <Link href="/" legacyBehavior>
               <a className="w-32 mb-4 md:mb-0">
                 <img
-                  src="/HeadstrongLogo2.svg"
+                  src="/HeadstrongLogo.svg"
                   alt="Headstrong Salon"
-                  className="h-64 w-auto"
+                  className="h-20 md:h-32 w-auto"
                 />
               </a>
             </Link>
-            <div className="flex flex-col md:flex-row md:items-center">
+            <div className="flex flex-col md:flex-row md:items-center md:ml-4">
               <Link href="/about" legacyBehavior>
-                <a className="mr-4">About</a>
+                <a className="my-1 md:my-0 md:mr-4">About</a>
               </Link>
               <Link href="/contact" legacyBehavior>
-                <a className="mr-4">Contact</a>
+                <a className="my-1 md:my-0 md:mr-4">Contact</a>
               </Link>
               <Link href="/privacy" legacyBehavior>
-                <a className="mr-4">Privacy</a>
+                <a className="my-1 md:my-0 md:mr-4">Privacy</a>
               </Link>
               <Link href="/terms" legacyBehavior>
-                <a className="mr-4">Terms</a>
+                <a className="my-1 md:my-0">Terms</a>
               </Link>
             </div>
           </div>
-          <div className="flex items-center">
+          <div className="flex items-center mt-4 md:mt-0">
             <a
               href="https://www.facebook.com"
               target="_blank"
               rel="noopener noreferrer"
-              className="mr-4"
+              className="mr-4 text-2xl md:text-3xl"
               aria-label="Facebook"
             >
               <FaFacebookF />
@@ -47,7 +47,7 @@ export default function Footer() {
               href="https://www.instagram.com"
               target="_blank"
               rel="noopener noreferrer"
-              className="mr-4"
+              className="mr-4 text-2xl md:text-3xl"
               aria-label="Instagram"
             >
               <FaInstagram />
@@ -56,7 +56,7 @@ export default function Footer() {
               href="https://www.twitter.com"
               target="_blank"
               rel="noopener noreferrer"
-              className="mr-4"
+              className="mr-4 text-2xl md:text-3xl"
               aria-label="Twitter"
             >
               <FaTwitter />
