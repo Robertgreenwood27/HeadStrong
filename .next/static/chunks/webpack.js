48eb984bb17685c17e5a718d704b25a49b637242
Robert Greenwood <RobertGreenwood27@gmail.com>
2024-03-04 06:47:48 -0700

    added meta tag

--- .next/static/chunks/webpack.js
+++ .next/static/chunks/webpack.js
@@ -183,7 +183,7 @@
 /******/ 	
 /******/ 	/* webpack/runtime/getFullHash */
 /******/ 	!function() {
-/******/ 		__webpack_require__.h = function() { return "dbef6273e34ad551"; }
+/******/ 		__webpack_require__.h = function() { return "8457642976813d7b"; }
 /******/ 	}();
 /******/ 	
 /******/ 	/* webpack/runtime/global */
--- pages/_app.js
+++ pages/_app.js
@@ -2,19 +2,21 @@ import "../styles.css";
 import 'react-masonry-css';
 import Header from "../components/Header";
 import Footer from "../components/Footer";
-
-
+import Head from 'next/head'; // Import Head from next/head
 
 // This default export is required in a new `pages/_app.js` file.
 export default function MyApp({ Component, pageProps }) {
   return (
-    <div  className="bg-zinc-900 text-zinc-300">
-<Header/>
-<div className="py-16">
-  <Component {...pageProps} />
-</div>
-
-<Footer/>
-</div>
+    <div className="bg-zinc-900 text-zinc-300">
+      <Head>
+        <meta name="viewport" content="width=device-width, initial-scale=1" />
+        <title>Your Site Title</title> {/* You can also set a default title for all pages here */}
+      </Head>
+      <Header />
+      <div className="py-16">
+        <Component {...pageProps} />
+      </div>
+      <Footer />
+    </div>
   );
 }
